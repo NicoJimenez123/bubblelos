788911615771eb023e9884a424042cc6d88204b8
Nicolas Jimenez <nicolasdanieljimenez@hotmail.com>
2025-01-22 15:24:18 -0300

    Añadida importación

--- jugador.gd
+++ jugador.gd
@@ -1,22 +1,26 @@
 extends AnimatedSprite2D
 
+var mensajes = preload('res://mensajes.gd').new()
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	print(mensajes.msg())
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 func _input(event):
+	if (event is InputEventMouseButton && event.button_index == MOUSE_BUTTON_LEFT):
+		print(mensajes.msg())
 	if event is InputEventMouseMotion:
 		mover(event.position)
-	else:
+	"""else:
 		if (event.button_index == MOUSE_BUTTON_LEFT):
 			print(position)
 			print(event.position)
 			estirar(event.position)
+	"""
 		
 func estirar(pos):
 	var x = pos.x
