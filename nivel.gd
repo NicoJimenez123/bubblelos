1a9bc64370d5282d8d101c44bc14c29c5cba2c00
Nicolas Jimenez <nicolasdanieljimenez@hotmail.com>
2025-01-22 22:19:50 -0300

    Implementada reproducción de sonido

--- nivel.gd
+++ nivel.gd
@@ -4,7 +4,27 @@ extends Node2D
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.is_pressed() and event.keycode == KEY_M:
+			toogleBackground()
+
+var sonido = false
+
+func toogleBackground() -> void:
+	if sonido:
+		pararSonido()
+		sonido = false
+	else:
+		reproducir()
+		sonido = true
+
+func reproducir() -> void:
+	$AudioStreamPlayer.play()
+
+func pararSonido() -> void:
+	$AudioStreamPlayer.stop()
