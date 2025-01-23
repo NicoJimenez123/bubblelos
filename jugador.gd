348191d9987bdd9ce46bf853950cc5cb31336d2e
Angel G300 Saucedo <saucedog926@gmail.com>
2025-01-22 21:37:14 -0300

    movimiento fluido

--- jugador.gd
+++ jugador.gd
@@ -1,70 +1,46 @@
 extends Sprite2D
 
+@export var move_speed : float = 10.0
+
 var burbuja = preload('res://burbujaEstirar.gd').new()
-const ESTADOS: Dictionary = {'estado1': 'posicion_burbuja_para_editar_PNG.webp',
+const ESTADOS: Dictionary = {
+	'estado1': 'posicion_burbuja_para_editar_PNG.webp',
 	'estado2': 'posicion_burbuja_normal_PNG.webp',
 	'estado3': 'posicion_burbuja_estirada_PNG.webp',
 	'estado4': 'posicion_burbuja_por_reventar_PNG.webp',
 	'estado5': 'posicion_burbuja_rota_PNG.webp',
-	}
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+}
 
 var clickeando = false
-var clickStartX = 0
-var clickStartY = 0
-var clickEndX = 0
-var clickEndY = 0
+var click_start_position = Vector2()
+var current_impulse = Vector2()
+var current_velocity = Vector2()
+
+func _ready() -> void:
+	#Fijamos la escala por defecto.
+	scale = Vector2(1,1)
+	pass
+
+func _physics_process(delta: float) -> void:
+	# Aplica el impulso a la posición de la burbuja
+	position += current_velocity * delta
+	current_velocity = current_velocity * 0.9
+	if abs(current_velocity.x) < 0.1 and abs(current_velocity.y) < 0.1: # Si la velocidad es menor a 0.1
+		current_velocity = Vector2.ZERO #Detenemos la burbuja
+
 
 func _input(event):
 	if Input.is_key_pressed(KEY_R):
 		position.x = 160
 		position.y = 160
-	if event is InputEventMouseMotion:
-		#print(event)
-		#mover(event.position)
-		pass
 	if event is InputEventMouseButton:
 		if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+			# Se presiona el click, guardamos la posición
 			clickeando = true
-			# Guardar posición desde donde se clickeó
-			clickStartX = event.position.x
-			clickStartY = event.position.y
-			var resultado = (get_process_delta_time() + 1) * (clickStartX + clickStartY)
-		if event.is_released() and event.button_index == MOUSE_BUTTON_LEFT:
+			click_start_position = get_global_mouse_position()
+		if !event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+			# Se suelta el click, calculamos y aplicamos el impulso
 			clickeando = false
-			clickEndX = event.position.x
-			clickEndY = event.position.y
-		if !clickeando:
-			mover({'x': (clickEndX - clickStartX), 'y': (clickEndY - clickStartY)})
-	if clickeando && event is InputEventMouseMotion:
-		pass
-		
-	if Input.is_key_pressed(KEY_1):
-		self.texture = burbuja.cargarSprite(ESTADOS.estado1)
-	if Input.is_key_pressed(KEY_2):
-		self.texture = burbuja.cargarSprite(ESTADOS.estado2)
-	if Input.is_key_pressed(KEY_3):
-		self.texture = burbuja.cargarSprite(ESTADOS.estado3)
-	if Input.is_key_pressed(KEY_4):
-		self.texture = burbuja.cargarSprite(ESTADOS.estado4)
-	if Input.is_key_pressed(KEY_5):
-		self.texture = burbuja.cargarSprite(ESTADOS.estado5)
-	
-func estirar(pos):
-	var x = pos.x
-	var y = pos.y
-	scale.x = 0.01 * (x - position.x)
-	#scale.y = 0.0001 * (x - position.x)
-
-func mover(pos):
-	var x = pos.x
-	var y = pos.y
-	position.x += x
-	position.y += y
+			var current_mouse_position = get_global_mouse_position()
+			var impulse_vector = current_mouse_position - click_start_position
+			current_velocity = impulse_vector * move_speed
