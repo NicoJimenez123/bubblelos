1cc3826752db344fe8416978956b0c25023c73a2
Game Jam <gamejam@hotmail.com>
2025-01-25 16:39:55 -0300

    Casi terminado v2

--- video_stream_player.gd
+++ video_stream_player.gd
@@ -2,15 +2,14 @@ extends VideoStreamPlayer
 
 func _ready():
 	# Assuming the function you want to connect to is named "_on_VideoStreamPlayer_finished"
-	#connect("finished", self, "_on_VideoStreamPlayer_finished")  # This line is corrected
 	pass
-	
-func _on_VideoStreamPlayer_finished():
-	queue_free()
-	get_tree().change_scene_to_file("res://icon.tscn")
-	return self
 
 func _process(delta: float) -> void:
 	if Input.is_key_pressed(KEY_SPACE):
-		queue_free()
 		get_tree().change_scene_to_file("res://icon.tscn")
+		queue_free()
+
+
+func _on_finished() -> void:
+	get_tree().change_scene_to_file("res://icon.tscn")
+	queue_free()
