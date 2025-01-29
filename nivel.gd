23567e3733400f80633ef902876e22ad47030ea1
Facundo Mentoro <127545387+Veik1@users.noreply.github.com>
2025-01-29 20:46:13 -0300

    just an update

--- nivel.gd
+++ nivel.gd
@@ -23,16 +23,19 @@ func _process(delta: float) -> void:
 		game_over.visible = true
 
 func _input(event: InputEvent) -> void:
-	if Input.is_key_pressed(KEY_R):
-		enemigoAtacante1.reiniciar()
-		game_over.visible = false
-		win.visible = false
-		barra_oxigeno.reiniciar()
-		jugador.reiniciar()
+	if Input.is_key_pressed(KEY_R) or (event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT):
+		reiniciarJuego()
 	if event is InputEventKey:
 		if event.is_pressed() and event.keycode == KEY_M:
 			toogleBackground()
 var sonido = false
+
+func reiniciarJuego():
+	enemigoAtacante1.reiniciar()
+	game_over.visible = false
+	win.visible = false
+	barra_oxigeno.reiniciar()
+	jugador.reiniciar()
 
 func toogleBackground() -> void:
 	if sonido:
@@ -64,3 +67,7 @@ func on_player_win() -> void:
 func pause_all_nodes():
 	for child in root.get_children():
 		child.set_process(false)
+
+
+func _on_button_pressed() -> void:
+	reiniciarJuego()
--- video_stream_player.gd
+++ video_stream_player.gd
@@ -13,3 +13,8 @@ func _process(delta: float) -> void:
 func _on_finished() -> void:
 	get_tree().change_scene_to_file("res://icon.tscn")
 	queue_free()
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://icon.tscn")
+	queue_free()
